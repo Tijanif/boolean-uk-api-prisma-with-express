3a8bf4c412ec608b13e8ba800179e04bedb1dff9
Tijani Fulani <Fulani07@gmail.com>
2021-08-09 11:28:05 +0100

    find by Id and Create

--- src/resources/book/controller.js
+++ src/resources/book/controller.js
@@ -7,6 +7,36 @@ const getAllBooks = (req, res) => {
   });
 };
 
+// Find One By ID
+const findOnebyId = (req, res) => {
+  const id = Number(req.params.id);
+  prisma.book
+    .findUnique({
+      where: {
+        id: id,
+      },
+    })
+    .then((foundBook) => {
+      res.json({ foundBook });
+    });
+};
+
+// Create A Book
+const createABook = (req, res) => {
+  const newBook = req.body;
+  prisma.book
+    .create({
+      data: {
+        ...newBook,
+      },
+    })
+    .then((newBook) => {
+      res.json({ newBook });
+    });
+};
+
 module.exports = {
   getAllBooks,
+  createABook,
+  findOnebyId,
 };
--- src/resources/book/router.js
+++ src/resources/book/router.js
@@ -1,8 +1,15 @@
 const booksRouter = require('express').Router();
 
-const { getAllBooks } = require('./controller');
+const { getAllBooks, createABook, findOnebyId } = require('./controller');
 
+// Get All Books
 booksRouter.get('/', getAllBooks);
+
+//  Find one book
+booksRouter.get('/:id', findOnebyId);
+
+//  Create a book
+booksRouter.post('/', createABook);
 
 module.exports = {
   booksRouter,
